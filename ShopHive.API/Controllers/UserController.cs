071d761542dfbd168adf98a725ca3195c5bf8965
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-07-20 15:28:53 +0600

    added user controllers for authentication

---
+++ ShopHive.API/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopHive.API.Data;
+using ShopHive.API.Models.DTO;
+using ShopHive.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
+
+namespace ShopHive.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ShopHiveDbContext dbContext;
+
+        public UserController(ShopHiveDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var Users = await dbContext.Users.ToListAsync();
+
+            return Ok(Users);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> GetUser(int id) { 
+            var user = await dbContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        
+    }
+}
